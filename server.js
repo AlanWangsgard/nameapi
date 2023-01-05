dc3487a0893891310282abeb727ce9a4a99f1735
alanwangsgard <wan20003@byui.edu>
2023-01-05 11:59:32 -0800

    initial

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use("/", require("./routes"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
