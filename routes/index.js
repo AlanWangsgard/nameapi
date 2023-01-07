66f4f3df4db30a1d848a2fa01fb0f9a16c8cc558
alanwangsgard <wan20003@byui.edu>
2023-01-06 16:47:35 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ routes.get('/', (req, res) => {
     res.send('Alan Wangsgard')
   })
 
+routes.get('/another', (req, res) => {
+  res.send("Alan Wangsgard, but different")
+})
+
   module.exports = routes
