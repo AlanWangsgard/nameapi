d1a14d90ca61a0280fd559097c68cb3051b61073
alanwangsgard <wan20003@byui.edu>
2023-01-14 16:53:44 -0800

    new stuff

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const http = require('http');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const mongodb = require('./databases/connect');
 
 app.use("/", require("./routes"))
+app.use("/contacts", require("./routes/contacts"))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
