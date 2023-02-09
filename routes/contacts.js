c0b8ea82f2b0cd9fa77e37f5b43cd405ee3a2ffd
alanwangsgard <wan20003@byui.edu>
2023-02-09 09:32:01 -0800

    validation

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,17 +1,29 @@
 const routes = require("express").Router()
 const controller = require("../controllers/contacts")
+const {addContactValidation} = require("../validation/validation")
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 routes.get("/", controller.getAll)
 
 routes.get("/:id", controller.getSingle)
 
-routes.post("/", controller.addContact)
+routes.post("/", addContactValidation ,controller.addContact)
+
+routes.use((err, req, res, next) => {
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+});
 
 routes.put("/:id", controller.updateContact)
 
